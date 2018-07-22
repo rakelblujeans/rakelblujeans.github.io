11ce2d0ff76a2bb18cc33c26c9275ca9fd1e76b1
Raquel Bujans <raquel.bujans@gmail.com>
2018-07-22 11:59:09 -0400

    Use webpack in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development', // argv.mode === 'development' ? 'development' : 'production',
+  mode: 'production',
   module: {
     rules: [
       {
